69d52b1cfd0d9469b2ceb4e83a870ce23d2d628f
iwizu <iwizu@hotmail.com>
2020-08-12 17:18:04 +0300

    New PackageManager class

---
+++ WebappVisualTester/Packaging/IPackageManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebappVisualTester.Packaging
+{
+    public interface IPackageManager
+    {
+        void CreatePackageFile(string filename);
+
+        void CreatePackageWithProjectFile(string filename, string projectJson);
+    }
+}
---
+++ WebappVisualTester/Packaging/PackageManager.cs
@@ -0,0 +1,56 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WebappVisualTester.Packaging
+{
+    public class PackageManager : IPackageManager
+    {
+        public void CreatePackageFile(string filename)
+        {
+            using (var fs = File.Create(filename))
+            {
+            }
+        }
+
+        public void CreatePackageWithProjectFile(string filename, string projectJson)
+        {
+            using (var fs = File.Create(filename))
+            using (var outStream = new ZipOutputStream(fs))
+            {
+                outStream.PutNextEntry(new ZipEntry(Path.GetFileNameWithoutExtension(filename) + ".json"));
+
+                using (var sw = new StreamWriter(outStream))
+                {
+                    sw.Write(projectJson);
+                }
+            }
+        }
+
+        public void AddFileToPackage(string packageFilename, string filename, byte[] content)
+        {
+            using (var fs = File.Create(filename))
+            using (var outStream = new ZipOutputStream(fs))
+            {
+                outStream.PutNextEntry(new ZipEntry(Path.GetFileNameWithoutExtension(filename) + ".json"));
+
+                using (var sw = new StreamWriter(outStream))
+                {
+                    //sw.Write(projectJson);
+                }
+            }
+        }
+
+        private string GetFilenameWithoutPathAndExtension(string filename)
+        {
+            return Path.GetFileNameWithoutExtension(filename);
+        }
+
+        private string GetOnlyDirectory(string filename)
+        {
+            return Path.GetDirectoryName(filename);
+        }
+    }
+}
